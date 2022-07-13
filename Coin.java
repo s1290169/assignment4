0f1e854f338ef963909412c22706a3af02b33b3a
s1290169 <s1290169@u-aizu.ac.jp>
2022-07-13 19:51:28 +0900

    HoT :initial revision

---
+++ Coin.java
@@ -0,0 +1,36 @@
+public class Coin{
+    public static void main(String[] args){
+        int a = (int)(Math.random()*2);
+        int b = (int)(Math.random()*2);
+        int c = (int)(Math.random()*2);
+        int count1 = 0;
+        int count2 = 0;
+        System.out.println("Tossing a coin...");
+       
+        if(a == 0){
+            System.out.println("Round 1: Heads");
+            count1++;
+        }
+        else if(a == 1){
+            System.out.println("Round 1: Tails");
+            count2++;
+        }
+        if(b == 0){
+            System.out.println("Round 2: Heads");
+            count1++;
+        }
+        else if(b == 1){
+            System.out.println("Round 2: Tails");
+            count2++;
+        }
+        if(c == 0){
+            System.out.println("Round 3: Heads");
+            count1++;
+        }
+        else if(c == 1){
+            System.out.println("Round 3: Tails");
+            count2++;
+        }
+        System.out.println("Heads: "+count1+", Tails: "+count2);
+    }
+}
